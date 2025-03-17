17cc651bd4c29e4225b1bb06ca113974eea4907c
roopak <roopaksai5577@gmail.com>
2025-03-17 19:55:28 +0530

    courseinfo updated

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({course}) => {
+  console.log(course)
+  return (
+    <h1>{course}</h1>
+  )
+}
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+const Content = ({parts}) => {
+  return(
+    <div>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of Exercises {total}</p>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+  
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a Component',
+      exercises: 14
+    }]
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
   )
 }
 
