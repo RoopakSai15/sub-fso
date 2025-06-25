408c5d503c166d11d000353053165a2cbd32c6ad
roopak <roopaksai5577@gmail.com>
2025-06-25 10:32:11 +0530

    lint changes

--- part3/mongo.js
+++ part3/mongo.js
@@ -39,7 +39,7 @@ if (process.argv.length === 3){
     number: `${number}`,
   })
 
-  person.save().then(result => {
+  person.save().then(() => {
     console.log(`added ${person.name} number ${person.number} to phonebook!`)
     mongoose.connection.close()
   })
