e6d84e054a2678b41d36e13d833fd255c5cef7cb
roopak <roopaksai5577@gmail.com>
2025-06-27 22:00:03 +0530

    Excercise 4.1-4.2

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,18 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get("/", (request, response, next) => {
+  Blog.find({}).then((result) => {
+    response.json(result)
+  })
+})
+
+blogRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = blogRouter
